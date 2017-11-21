ce6107e127224da9b099d542ccca459c02525ceb
Daniel Jordi Perez <danipq26@gmail.com>
2017-11-21 13:13:43 +0100

    Reformated code files

--- src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
+++ src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
@@ -6,8 +6,8 @@ import org.junit.Test;
 
 public class SomeTest {
 
-	@Test
-	public void true_is_true() throws Exception {
-		assertTrue(false);
-	}
+    @Test
+    public void true_is_true() throws Exception {
+        assertTrue(false);
+    }
 }
