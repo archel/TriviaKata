2c12c4b62278258bcfbbebc691fe2b2915bfa249
Daniel Jordi Perez <danipq26@gmail.com>
2017-11-22 12:42:42 +0100

    Refactored test of penalty box

--- src/test/java/com/adaptionsoft/games/trivia/PenaltyBoxShould.java
+++ src/test/java/com/adaptionsoft/games/trivia/PenaltyBoxShould.java
@@ -1,32 +1,42 @@
 package com.adaptionsoft.games.trivia;
 
 import com.adaptionsoft.games.uglytrivia.PenaltyBox;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class PenaltyBoxShould {
+    private static final String PLAYER_ONE = "PLAYER_ONE";
+    private static final String PLAYER_TWO = "PLAYER_TWO";
 
-    @Test
-    public void at_first_it_dont_have_players_inside() {
-        PenaltyBox penaltyBox = new PenaltyBox();
-        assertFalse(penaltyBox.isIn("PLAYER_NAME"));
+    private PenaltyBox penaltyBox;
+
+    @Before
+    public void setUp() throws Exception {
+        penaltyBox = new PenaltyBox();
     }
 
     @Test
-    public void adds_a_player_to_penalty_box() {
-        PenaltyBox penaltyBox = new PenaltyBox();
-        penaltyBox.add("PLAYER_NAME");
-        assertTrue(penaltyBox.isIn("PLAYER_NAME"));
+    public void have_no_players_inside() {
+        assertFalse(penaltyBox.isIn(PLAYER_ONE));
     }
 
     @Test
-    public void get_out_a_player_from_penalty_box() {
-        PenaltyBox penaltyBox = new PenaltyBox();
-        penaltyBox.add("PLAYER_NAME");
-        penaltyBox.remove("PLAYER_NAME");
+    public void add_a_player_to_penalty_box() {
+        penaltyBox.add(PLAYER_ONE);
 
-        assertFalse(penaltyBox.isIn("PLAYER_NAME"));
+        assertTrue(penaltyBox.isIn(PLAYER_ONE));
+    }
+    
+    @Test
+    public void remove_second_player_from_penalty_box() {
+        penaltyBox.add(PLAYER_ONE);
+        penaltyBox.add(PLAYER_TWO);
+
+        penaltyBox.remove(PLAYER_TWO);
+
+        assertFalse(penaltyBox.isIn(PLAYER_TWO));
     }
 }
